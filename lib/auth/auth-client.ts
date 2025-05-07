9bf1f045d86629e97623dff641df012a3f2a7f04
JosSainson <121938088+jos34000@users.noreply.github.com>
2025-05-07 19:00:23 +0200

    fix(auth): mise à jour de l'URL de base du client d'authentification
    - Changement de l'URL de base du client d'authentification de "http://localhost:3000" à "https://jossainson.dev" pour refléter le nouveau domaine de production.

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -4,6 +4,6 @@ import { createAuthClient } from "better-auth/react"
 import { auth } from "./auth"
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://jossainson.dev",
   plugins: [inferAdditionalFields<typeof auth>()],
 })
